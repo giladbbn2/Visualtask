9c3e8888ead63d51504013e2acafb9f70df90f16
unknown <giladbbn2@gmail.com>
2019-04-27 21:22:04 +0300

    vt

--- 0.2/test/1/server.php
+++ 0.2/test/1/server.php
@@ -8,4 +8,17 @@ include dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . "server" . D
 autoload("\\VisualTask\\DB\\MysqlDB");
 autoload("\\VisualTask\\VisualTask\\MysqlVisualTask");
 
+$mysqldb = new \VisualTask\DB\MysqlDB(array(
+	"conn1" => array(
+		"host" => "localhost",
+		"user" => "db_user1",
+		"pass" => "123456abcdeFGH",
+		"db_name" => "test",
+		"port" => 3306
+	)
+));
+
+$db = $mysqldb->connect("conn1");
+$db->set_fetch_assoc(false);
+
 echo "123";
