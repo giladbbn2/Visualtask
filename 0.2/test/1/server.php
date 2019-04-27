cf2164cfe6ac7b7bdc95171e32fedb011d4e0e07
unknown <giladbbn2@gmail.com>
2019-04-27 21:46:36 +0300

    vt

--- 0.2/test/1/server.php
+++ 0.2/test/1/server.php
@@ -3,43 +3,48 @@
 error_reporting( E_ALL );
 ini_set('display_errors', 1);
 
-// include the vt_autoload_class function to load visualtask files properly
-include dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . "server" . DIRECTORY_SEPARATOR . "php" . DIRECTORY_SEPARATOR . "autoload.php";
 
-// this makes sure all necessary files are loaded properly
-vt_autoload_class("\\VisualTask\\DB\\MysqlDB");
-vt_autoload_class("\\VisualTask\\VisualTask\\MysqlVisualTask");
+if (isset($_POST["options"])){
 
-$mysqldb = new \VisualTask\DB\MysqlDB(array(
-	"conn1" => array(
-		"host" => "localhost",
-		"user" => "root",
-		"pass" => "004f0fec7eb37f3a0d735464dc6c0e0d",
-		"db_name" => "test",
-		"port" => 3306
-	)
-));
+    // include the vt_autoload_class function to load visualtask files properly
+    include dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . "server" . DIRECTORY_SEPARATOR . "php" . DIRECTORY_SEPARATOR . "autoload.php";
 
-$db = $mysqldb->connect("conn1");
-$db->set_fetch_assoc(false);
+    // this makes sure all necessary files are loaded properly
+    vt_autoload_class("\\VisualTask\\DB\\MysqlDB");
+    vt_autoload_class("\\VisualTask\\VisualTask\\MysqlVisualTask");
 
-// init visualtask
-$vt = new \VisualTask\VisualTask\MysqlVisualTask();
-$vt->resource_types = array(
-	// exposed resource type name => real resource type in visualtask
-	"res1" => "mysql"
-);
-$vt->mysql_db = $db;
-$vt->limit_size_default = 10;
-$vt->limit_size_max = 100;
+    $mysqldb = new \VisualTask\DB\MysqlDB(array(
+        "conn1" => array(
+            "host" => "localhost",
+            "user" => "root",
+            "pass" => "004f0fec7eb37f3a0d735464dc6c0e0d",
+            "db_name" => "test",
+            "port" => 3306
+        )
+    ));
 
-// this file returns ajax to consumers requesting visualtask
+    $db = $mysqldb->connect("conn1");
+    $db->set_fetch_assoc(false);
 
-// load preset
-include "preset1.php";
+    // init visualtask
+    $vt = new \VisualTask\VisualTask\MysqlVisualTask();
+    $vt->resource_types = array(
+        // exposed resource type name => real resource type in visualtask
+        "res1" => "mysql"
+    );
+    $vt->mysql_db = $db;
+    $vt->limit_size_default = 10;
+    $vt->limit_size_max = 100;
 
-$options = $vt->preset(new preset1(), $_POST["options"]);
+    // this file returns ajax to consumers requesting visualtask
 
-header('Content-type: application/json');
-echo json_encode($options);
-die();
+    // load preset
+    include "preset1.php";
+
+    $options = $vt->preset(new preset1(), $_POST["options"]);
+
+    header('Content-type: application/json');
+    echo json_encode($options);
+    die();
+
+}
